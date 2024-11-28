113a0083879103efce368b0df654fe359020fd98
Marius Winterscheid <44503013+MariusWinterscheid@users.noreply.github.com>
2024-11-28 21:57:05 +0100

    next chapter

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SinglefruitComponent } from './singlefruit/singlefruit.component';
 
 @Component({
   selector: 'app-fruitlist',
-  imports: [CommonModule],
+  imports: [CommonModule, SinglefruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
 export class FruitlistComponent {
+
+  fontColorGood = 'green';
+  fontColorBad = 'red';
 
   fruitlist = [
     {
